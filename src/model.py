fb69702ee857f1edef2efc3f5d98cf0ebaf49b06
arunrakhelkar <arunrakhelkar@gmail.com>
2019-07-29 23:13:17 +0530

    added categories

--- src/model.py
+++ src/model.py
@@ -9,54 +9,60 @@ from sklearn.metrics.pairwise import cosine_similarity
 class Model:
  
     def __init__(self, persona, n = 5):
-
         self.n = n
         self.persona = persona
         self.users = User().list({'persona':persona})
         self.products = Product().list()
-        self.user_ids = [ usr.get('email_id') for usr in self.users ]
-        self.product_ids = [ str(prd.get('_id')) for prd in self.products ]
-        self.matrix = self.get_matrix()
 
     def update_matrix_values(self, email_id, a, col):
-
         usr_fbs = list(DB().find(DB.USER_FB,{'email_id':email_id}))
         for fb in usr_fbs:
             index = self.product_ids.index(fb.get('product_id'))
             a[col][index] = fb.get('value')
 
     def get_matrix(self):
-
         a = np.zeros(shape = (len(self.users), len(self.products)))
+        self.product_ids = [ str(prd.get('_id')) for prd in self.products ]
+        self.user_ids = [ usr.get('email_id') for usr in self.users ]
         for i, usr in enumerate(self.users):
             self.update_matrix_values(usr.get('email_id'), a, i)
-        df = pd.DataFrame(a, index = self.user_ids, columns = self.product_ids)
-        return df
-    
-    def get_similar_users(self, email_id):
+        return pd.DataFrame(a, index=self.user_ids, columns=self.product_ids)
 
-        cosim = cosine_similarity(self.matrix, self.matrix)
+    def get_ctgr_mtrx(self, mtrx):
+        ctgrs = [prd.get('category') for prd in self.products]
+        ctgr_mtrx = mtrx.copy()
+        ctgr_mtrx.columns = ctgrs
+        return ctgr_mtrx.groupby(level=0, axis=1).mean()
+
+    def get_similar_users(self, email_id, mtrx):
+        cosim = cosine_similarity(mtrx, mtrx)
         index = self.user_ids.index(email_id)
         usrs = list(enumerate(cosim[index]))
         smlr_usrs = sorted(usrs, key=lambda x:x[1], reverse=True)[1:self.n]
-        smlr_usrs = [x[0] for x in smlr_usrs]
-        return smlr_usrs
-    
+        return [self.user_ids[x[0]] for x in smlr_usrs]
+
+    def get_product_means(self, email_id, mtrx):
+        smlr_usrs = self.get_similar_users(email_id, mtrx)
+        usr_prod = mtrx.loc[smlr_usrs,:]
+        return pd.Series(usr_prod.mean(axis=0))
+        #normalise mean scores
+
+    def get_ctgr_means(self, email_id, ctgr_mtrx):
+        smlr_usrs = self.get_similar_users(email_id, ctgr_mtrx)
+        usr_ctgr = ctgr_mtrx.loc[smlr_usrs, :]
+        return pd.Series(usr_ctgr.mean(axis=0))
+
     def get_recomended_product(self, email_id):
         
-        smlr_usrs = self.get_similar_users(email_id)
-        smlr_usrs = [self.user_ids[i] for i in smlr_usrs]
-        candidates_prod = self.matrix.loc[smlr_usrs,:]
-        mean_score = pd.Series(candidates_prod.mean(axis=0))
-        mean_score = mean_score.sort_values(axis=0, ascending=False)
-        recom_prod = list(mean_score.iloc[:].keys())
-        index = self.user_ids.index(email_id)
-        for i in recom_prod:
-            if int(self.matrix.iloc[index][i]) != 0: continue
-            return self.products[self.product_ids.index(i)] 
-        #No product remaining
+        mtrx = self.get_matrix()
+        print(mtrx)
+        prod_mean = self.get_product_means(email_id, mtrx)
+        ctgr_mtrx = self.get_ctgr_mtrx(mtrx)
+        ctgr_mean = self.get_ctgr_means(email_id, ctgr_mtrx)
+        print(prod_mean)
+        print(ctgr_mean)
         return None
  
 # Dev mode !!
-# a = Model('Software Developer')
-# a.get_recomended_product('arunrakhelkar@gmail.com')
+a = Model('Software Developer')
+a.get_recomended_product('arunrakhelkar@gmail.com')
